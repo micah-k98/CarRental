b87b9b503963598131d88b38a3c8f8df73ca1b26
micah-k98 <146677653+micah-k98@users.noreply.github.com>
2023-10-26 16:20:52 -0700

    Added conditions if any of the checkbox options were checked

--- scripts/index.js
+++ scripts/index.js
@@ -14,10 +14,25 @@ function estimateButtonClicked()
     rental.pickupDate = document.getElementById("pickupDateInput").value;
     rental.numOfDays = document.getElementById("numOfDaysInput").value;
 
-
     //rule
     rental.carRentalPerDay = 29.99;
     rental.carRentalCost = calculateCarRentalPerDay(rental)
+
+    //checkboxes for options
+    const tollTag = document.getElementById("tollTag");
+    const gps = document.getElementById("gps");
+    const roadsideAssistance = document.getElementById("roadsideAssistance");
+
+    rental.optionsCost = 0;
+    if (tollTag.checked) rental.optionsCost += rental.numOfDays * 3.95;
+    if (gps.checked) rental.optionsCost += rental.numOfDays * 2.95;
+    if (roadsideAssistance.checked) rental.optionsCost += rental.numOfDays * 2.95;
+
+
+
+
+
+    
 
     
 
@@ -31,5 +46,6 @@ function calculateCarRentalPerDay(rental)
 
 function displayOutput(rental)
 {
-    document.getElementById("carRentalCost").innerText = rental.carRentalCost;
+    document.getElementById("carRentalCost").innerText = (rental.carRentalCost).toFixed(2);
+    document.getElementById("optionsCost").innerText = (rental.optionsCost).toFixed(2);
 }

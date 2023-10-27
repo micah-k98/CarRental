fa18d9da57dd49e9282c672252fc6c029c02f983
micah-k98 <146677653+micah-k98@users.noreply.github.com>
2023-10-27 01:04:41 -0700

    Modified and added functions

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,12 @@
 "use strict"
 
+/**
+ * Rules for determining the rental charges:
+ * - basic car rental is $29.99 per day
+ * - there is a 30% surcharge on the basic car rental for drivers under 25
+ */
+
+
 window.onload = function()
 {
     const estimateButton = document.getElementById("estimateButton");
@@ -14,44 +21,62 @@ function estimateButtonClicked()
     rental.pickupDate = document.getElementById("pickupDateInput").value;
     rental.numOfDays = document.getElementById("numOfDaysInput").value;
 
-    //rule
-    rental.carRentalPerDay = 29.99;
-    rental.carRentalCost = calculateCarRentalPerDay(rental)
+    //car rental cost
+    rental.carRentalCost = calculateCarRentalPerDay(rental);
 
     //checkboxes for options
-    const tollTag = document.getElementById("tollTag");
-    const gps = document.getElementById("gps");
-    const roadsideAssistance = document.getElementById("roadsideAssistance");
-
-    rental.optionsCost = 0;
-    if (tollTag.checked) rental.optionsCost += rental.numOfDays * 3.95;
-    if (gps.checked) rental.optionsCost += rental.numOfDays * 2.95;
-    if (roadsideAssistance.checked) rental.optionsCost += rental.numOfDays * 2.95;
+    rental.optionsCost = calculateCheckedRentalOptions(rental);
 
     //under 25 radio button
-    const notUnder25 = document.getElementById("notUnder25");
-    const under25 = document.getElementById("under25");
-
-    rental.under25SurchargeCost = 0;
-    if (under25.checked) rental.under25SurchargeCost = rental.carRentalCost * .30; //30% surcharged based on the instructions
+    rental.under25SurchargeCost = calculateCheckedSurcharge(rental);
 
     //total cost
     rental.totalCost = +(rental.carRentalCost + rental.optionsCost + rental.under25SurchargeCost).toFixed(2);
 
-    
 
     displayOutput(rental);
 }
 
 function calculateCarRentalPerDay(rental)
 {
-    return +(rental.carRentalPerDay * rental.numOfDays).toFixed(2);
+    const carRentalPerDay = 29.99;
+    return +(carRentalPerDay * rental.numOfDays).toFixed(2);
+}
+
+function calculateCheckedRentalOptions(rental)
+{
+    const tollTag = document.getElementById("tollTag");
+    const gps = document.getElementById("gps");
+    const roadsideAssistance = document.getElementById("roadsideAssistance");
+
+    rental.optionsCost = 0.00;
+    if (tollTag.checked) rental.optionsCost += rental.numOfDays * 3.95;
+    if (gps.checked) rental.optionsCost += rental.numOfDays * 2.95;
+    if (roadsideAssistance.checked) rental.optionsCost += rental.numOfDays * 2.95;
+
+    return +(rental.optionsCost).toFixed(2);
+}
+
+function calculateCheckedSurcharge(rental)
+{
+    const under25 = document.getElementById("under25");
+
+    rental.under25SurchargeCost = 0.00;
+    if (under25.checked) rental.under25SurchargeCost = rental.carRentalCost * .30; //30% surcharged based on the instructions
+
+    return +(rental.under25SurchargeCost).toFixed(2);
 }
 
 function displayOutput(rental)
 {
     document.getElementById("carRentalCost").innerText = rental.carRentalCost;
-    document.getElementById("optionsCost").innerText = (rental.optionsCost).toFixed(2);
-    document.getElementById("under25SurchargeCost").innerText = (rental.under25SurchargeCost).toFixed(2);
+    
+    //if else so that the output is not just "0" but "0.00"
+    if (rental.optionsCost == 0) document.getElementById("optionsCost").innerText = "0.00";
+    else document.getElementById("optionsCost").innerText = rental.optionsCost;
+
+    if (rental.under25SurchargeCost == 0) document.getElementById("under25SurchargeCost").innerText = "0.00";
+    else document.getElementById("under25SurchargeCost").innerText = rental.under25SurchargeCost;
+
     document.getElementById("totalCost").innerText = rental.totalCost;
 }
